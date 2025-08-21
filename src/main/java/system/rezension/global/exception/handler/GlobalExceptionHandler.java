cf2342245d68e4d0debebd673bed83c170426b26
김연호 <kimfine000@gmail.com>
2025-08-21 18:12:15 +0900

    feat: add exception handler for MemberNotFoundException in GlobalExceptionHandler

--- src/main/java/system/rezension/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/system/rezension/global/exception/handler/GlobalExceptionHandler.java
@@ -3,6 +3,9 @@ package system.rezension.global.exception.handler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import system.rezension.domain.member.exception.MemberNotFoundException;
+import system.rezension.domain.studynote.exception.StudyNoteAccessDeniedException;
+import system.rezension.domain.studynote.exception.StudyNoteNotFoundException;
 import system.rezension.global.exception.CustomStatusException;
 import system.rezension.global.exception.dto.response.ErrorResponse;
 
@@ -10,6 +13,13 @@ import system.rezension.global.exception.dto.response.ErrorResponse;
 public class GlobalExceptionHandler {
     @ExceptionHandler(CustomStatusException.class)
     public ResponseEntity<ErrorResponse> handleClassCastException(CustomStatusException ex) {
+        return ResponseEntity
+                .status(ex.getStatus().getStatusCode())
+                .body(ErrorResponse.errorResponse(ex.getStatus()));
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleMemberNotFoundException(CustomStatusException ex) {
         return ResponseEntity
                 .status(ex.getStatus().getStatusCode())
                 .body(ErrorResponse.errorResponse(ex.getStatus()));
