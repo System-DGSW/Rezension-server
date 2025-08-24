d7a582fbe79b62a544b7d1dae926e8f97298c80e
김연호 <kimfine000@gmail.com>
2025-08-24 23:18:09 +0900

    feat: add findWithMemberById using EntityGraph to fetch StudyNote with member

--- src/main/java/system/rezension/domain/studynote/repository/StudyNoteRepository.java
+++ src/main/java/system/rezension/domain/studynote/repository/StudyNoteRepository.java
@@ -2,6 +2,7 @@ package system.rezension.domain.studynote.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import system.rezension.domain.member.entity.Member;
 import system.rezension.domain.studynote.entity.StudyNote;
@@ -12,7 +13,9 @@ import java.util.List;
 public interface StudyNoteRepository extends JpaRepository<StudyNote, Long> {
     List<StudyNote> findAllByMember(Member member);
 
+    @EntityGraph(attributePaths = {"member"})
     Page<StudyNote> findByMemberId(Long memberId, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"member"})
     Page<StudyNote> findByMemberIdAndVisibility(Long memberId, Visibility visibility, Pageable pageable);
 }
