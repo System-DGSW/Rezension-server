861b2bbecd5316527fd6a4766fa5857151abc8ce
김연호 <kimfine000@gmail.com>
2025-08-27 16:52:51 +0900

    feat: swagger + jwt

--- src/main/java/system/rezension/global/config/SwaggerConfig.java
+++ src/main/java/system/rezension/global/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package system.rezension.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,10 +12,23 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Rezension API")
+                .description("API 문서")
+                .version("v1.0.0");
+
+        String jwtSchemeName = "jwtAuth";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("Auth API")
-                        .description("회원 인증 관련 API 문서")
-                        .version("v1.0.0"));
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
     }
 }
